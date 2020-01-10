43a8061b942b856e08bfcc849a85ad75b2255fa5
Satyakee Sen <satyasen@houlws12.cluster.na.tgsn.org>
2020-01-10 15:54:48 -0600

    simple loader for loading the dataset

---
+++ loaders/dataloader.py
@@ -0,0 +1,32 @@
+################################################################################
+# laoder for fault data
+# randomly selects 15% of data in the dir to be set aside for validation set
+# easily modifiable to use user defined validation set (TODO)
+##############################################################################
+
+import os
+from sklearn.model_selection import train_test_split
+
+from torch.utils import data
+
+from .fault_dataset import FaultPrep
+
+def LoadData(training_data_path, file_list, image_size, split=0.15, workers=4, batch_size=1, transforms=None):
+    
+    if not os.path.exists(training_data_path):
+        error_message = 'Folder ' + os.path.abspath(training_data_path) + ' does not exist.'
+        raise OSError(error_message)
+    
+
+    training_samples, validation_samples = train_test_split(file_list, test_size=split, random_state=393939)   # do the train-val split here
+    
+    training_dataset = FaultPrep(training_data_path, training_samples, transforms=transforms)
+    validation_dataset = FaultPrep(training_data_path, validation_samples, transforms=None)
+    
+    training_loader = data.DataLoader(training_dataset, batch_size=batch_size, num_workers=workers, shuffle=True,pin_memory=True)
+    validation_loader = data.DataLoader(validation_dataset, batch_size=batch_size, num_workers=workers, shuffle=False, pin_memory=True)
+    
+    return training_loader, validation_loader
+    
+    
+    
---
+++ loaders/fault_dataset.py
@@ -0,0 +1,53 @@
+"""
+simple preprocess the fault training data and prep for loader
+Assumes data folder (root_path) has two dirs: images(with all the images), labels (with all the labels)
+All images and labels are numpy arrays (format : X,Y,Z)
+"""
+
+import os
+import numpy as np
+import imageio
+
+import torch
+from torch.utils import data
+
+class FaultPrep(data.Dataset):
+    
+    def __init__(self, root_path, file_list, transforms=None):
+        """
+        """
+        self.root = root_path
+        self.n_classes = 2
+            
+        self.file_list = file_list
+    
+    def __len__(self):
+        return len(self.file_list)
+    
+    def __getitem__(self, index):
+        
+        if index not in range(0, len(self.file_list)):
+            return self.__getitem__(np.random.randint(0, self.__len__()))
+            
+        file_id = self.file_list[index]
+        
+        image_folder = os.path.join(self.root, "images")
+        image_path = os.path.join(image_folder, file_id + ".npy")
+        
+        label_folder = os.path.join(self.root, "labels")
+        label_path = os.path.join(label_folder, file_id + ".npy")
+
+        # read image and label and  
+        image = np.load(image_path)   # X,Y,Z
+        image = (image - np.mean(image))/np.std(image)
+        image = image.transpose(2,1,0)  # Z,X,Y
+        image = np.expand_dims(image,0) # 1,Z,X,Y
+
+        label = np.load(label_path)
+        label = label.transpose(2,1,0)
+        label = np.expand_dims(label,0)
+
+        return image, label
+
+if __name__ == "__main__":
+    pass

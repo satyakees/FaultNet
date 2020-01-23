826bff0c7384576260f67284e18ac694fdcb1686
Satyakee Sen <satyasen@houlws12.cluster.na.tgsn.org>
2020-01-23 10:07:17 -0600

    support more models

--- models/load_models.py
+++ models/load_models.py
@@ -4,8 +4,9 @@ import torch
 from .vnet import VNet
 from .vnet_modified import VNet_modified
 from .unet_3D_Res1 import unet_3D_Res1
+from .unet_3D_DS import unet_3D_DS
 
-def getModel(model_arch, output_channels=2, parallel_flag=False, gpu_flag=True):
+def getModel(model_arch, output_channels=2, parallel_flag=False, gpu_flag=True,base_filter=32):
    
     print(" model arch specified by user is ",model_arch)
 
@@ -20,6 +21,10 @@ def getModel(model_arch, output_channels=2, parallel_flag=False, gpu_flag=True):
     elif model_arch.lower() == 'unet_3d_res1':
         print("loading UNet3d with 1 Residual block, output will have %d channels"%(output_channels))
         model = unet_3D_Res1()
+
+    elif model_arch.lower() == 'unet_3d_ds':
+        print("loading UNet3d with Deep supervision  output will have base-filter size %d"%(base_filter))
+        model = unet_3D_DS(n_classes=output_channels, base_n_filter=base_filter)
 
     else:
         
